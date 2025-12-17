8bec217a9071cb2c7783a56c356a800633eec5a2
Mark Rode <rode.mark01@gmail.com>
2025-12-17 10:42:45 +0300

    add signing

--- build.gradle.kts
+++ build.gradle.kts
@@ -183,6 +183,17 @@ publishing {
     }
 }
 
+signing {
+    isRequired = System.getenv().containsKey("ORG_GRADLE_PROJECT_signingKey") && System.getenv()
+        .containsKey("ORG_GRADLE_PROJECT_signingPassword")
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    sign(publishing.publications)
+}
+
+project.tasks.findByPath("publish")?.dependsOn(":artifactoryPublish")
+
 tasks.asciidoctor {
     sources {
         include("**/*.adoc")
